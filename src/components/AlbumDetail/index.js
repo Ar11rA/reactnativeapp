df01ffcb7bc60180f0559c75579119dba5cd62e9
Ar11rA <aritra.ghosh2295@yahoo.com>
2017-11-16 20:16:52 +0530

    added card component

--- src/components/AlbumDetail/index.js
+++ src/components/AlbumDetail/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {Image, Linking, View, Text } from 'react-native';
+import {Image, Linking, Text } from 'react-native';
+import Card from '../Card'
 const AlbumDetail = (props) => {
   return (
-    <View >
+    <Card>
       <Text>{props.album.title}</Text>
       <Text>{props.album.artist}</Text>
       <Text onPress={() => Linking.openURL(props.album.url)}>{props.album.url}</Text>
       <Image style={{width: 50, height: 50}} source={{uri: props.album.image}} />
-    </View>
+    </Card>
   );
 }
 
---
+++ src/components/Card/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import cardStyles from './Card.style'
+const Card = (props) => {
+  const { containerStyle } = cardStyles
+  return (
+    <View style={containerStyle}>
+      { props.children }
+    </View>
+  );
+}
+
+export default Card
