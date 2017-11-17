79b994cd764928e57da63a27f866028ec9f559b1
Ar11rA <aritra.ghosh2295@yahoo.com>
2017-11-17 17:28:26 +0530

    added coverage

--- src/components/Button/__tests__/Button.test.js
+++ src/components/Button/__tests__/Button.test.js
@@ -1,12 +1,25 @@
-import 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 import React from 'react';
 import Button from '../';
-
+import { shallow } from 'enzyme';
+import Enzyme from 'enzyme';
 import renderer from 'react-test-renderer';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 it('renders correctly', () => {
   const tree = renderer.create(
     <Button />
     ).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it('it should render 1 text component', () => {
+  const wrapper = shallow(<Button/>);
+  expect(wrapper.find(Text)).toHaveLength(1)
+});
+  
+it('it should render 1 touchable opacity component', () => {
+  const wrapper = shallow(<Button/>);
+  expect(wrapper.find(TouchableOpacity)).toHaveLength(1)
 });
