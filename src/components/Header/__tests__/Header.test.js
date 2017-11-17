c519a38e322076f406f3f06ca03e997515ef3529
Ar11rA <aritra.ghosh2295@yahoo.com>
2017-11-17 15:17:06 +0530

    added enzyme component tests

--- src/components/Header/__tests__/Header.test.js
+++ src/components/Header/__tests__/Header.test.js
@@ -1,12 +1,24 @@
-import 'react-native';
+import { View, Text } from 'react-native';
 import React from 'react';
 import Header from '../';
-
+import { shallow } from 'enzyme';
+import Enzyme from 'enzyme';
 import renderer from 'react-test-renderer';
-
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 it('renders correctly', () => {
   const tree = renderer.create(
     <Header />
     ).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it('it should render 1 view component', () => {
+  const wrapper = shallow(<Header/>);
+  expect(wrapper.find(View)).toHaveLength(1)
+});
+  
+it('it should render 1 text components', () => {
+  const wrapper = shallow(<Header/>);
+  expect(wrapper.find(Text)).toHaveLength(1)
 });
